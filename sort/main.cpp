74ce5549de154bd5d511ea9398d13059f7869644
police663 <yigeren010@gmail.com>
2021-06-16 08:12:24 -0700

    add InsertSort

--- sort/main.cpp
+++ sort/main.cpp
@@ -3,35 +3,54 @@
 
 using namespace std;
 
-class SelectSort{
+class InsertSort{
 public:
     static void sort(vector<int>& arr){
          for(uint32_t i=1; i<arr.size(); i++){
-             for(int j=i; j>0; j--){
-                 if(arr[j] < arr[j-1]){
-                     int tmp = arr[j];
+             int tmp = arr[i];
+             for(int j=i; j>0 && tmp < arr[j-1]; j--){
                      arr[j] = arr[j-1];
                      arr[j-1] = tmp;
-                 }
              }
          }
     }
 };
 
+// O(n^2)
 class BubbleSort{
 public:
     static void bsort(vector<int>& arr){
-         for(uint32_t i=0; i<arr.size()-1; i++){
-             for(uint32_t j=0; j<arr.size()-1-i; j++){
-                 if(arr[j] > arr[j+1]){
-                     int tmp = arr[j];
-                     arr[j] = arr[j+1];
-                     arr[j+1] = tmp;
-                 }
-             }
-         }
+        bool flag = true;
+        for(size_t i=0; i<arr.size(); ++i){
+            for(size_t j=0; j<arr.size()-1-i; ++j){
+                int tmp = arr[j];
+                if(arr[j] > arr[j+1]){
+                    flag = false;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+            if(flag)
+                break;
+            flag = true;
+        }
+
     }
 };
+//class BubbleSort{
+//public:
+//    static void bsort(vector<int>& arr){
+//         for(uint32_t i=0; i<arr.size()-1; i++){
+//             for(uint32_t j=0; j<arr.size()-1-i; j++){
+//                 if(arr[j] > arr[j+1]){
+//                     int tmp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = tmp;
+//                 }
+//             }
+//         }
+//    }
+//};
 
 class QuickSort{
 public:
@@ -57,15 +76,21 @@ public:
     }
 };
 
+
 int main()
 {
     vector<int> array{2,4,6,8,10,1,3,5,7,9};
-//    SelectSort::sort(array);
+    for(int i=0; i<80000; ++i){
+        array.push_back(rand());
+    }
+//    InsertSort::sort(array);
 //    BubbleSort::bsort(array);
     QuickSort::sort(array, 0, 10);
 
-    for(auto i : array){
+    for(int i : array){
         cout << i << endl;
     }
+
+    cout << 1 << endl;
     return 0;
 }
